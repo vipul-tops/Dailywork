7be4c6f4904a24b7424b11a4f611a53fce4da651
vipul-tops <vmmemakiya22@gmail.com>
2023-08-18 07:46:00 +0530

    17-08

---
+++ 17 aug.py
@@ -0,0 +1,18 @@
+for i in range(6):
+    print("*"*i)
+for i in range(6):
+    print("*"*(6-i))
+
+print("")
+
+for i in range(6):
+    print(" "*(6-i)," *"*i)
+for i in range(6):
+    print(" "*i," *"*(6-i))
+
+print("")
+
+for i in range(6):
+    print(" "*i," *"*(6-i))
+
+
